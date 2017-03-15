7db77a62bfbb839b8459d74852bcba09a92a7b5c
Paul Robertson <paul.robertson@simulation-systems.co.uk>
2017-03-15 17:46:57 +0000

    add cards api

--- cards.js
+++ cards.js
@@ -1,9 +1,10 @@
 var Cards = function() {
-  var cards = []
-    for (var shape = 0; shape < 3; shape++)
-      for (var color = 0; color < 3; color++)
-        for (var number = 0; number < 3; number++)
-          for (var fill = 0; fill < 3; fill++)
-            cards.push( {shape: shape, color: color, number: number, fill: fill})
+  var arr = []
+  for (var shape = 0; shape < 3; shape++)
+    for (var color = 0; color < 3; color++)
+      for (var number = 0; number < 3; number++)
+        for (var fill = 0; fill < 3; fill++)
+          arr.push( {shape: shape, color: color, number: number, fill: fill})
+  this.cards = arr
 }
 module.exports = Cards
--- controllers/api/cards.js
+++ controllers/api/cards.js
@@ -1,10 +1,12 @@
-var Room = require('../cards')
+var Cards = require('../../cards')
+var shuffle = require('lodash.shuffle')
 var router = require('express').Router()
 
 // return a collection of rooms
 router.get('/', function(req, res, next) {
-  var cards = new Cards()
-  res.json(cards)
+  var cards = new Cards().cards
+  var shuffledCards = shuffle(cards)
+  res.json(shuffledCards)
 })
 
 module.exports = router
