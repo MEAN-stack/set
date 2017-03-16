6692b8e4b5c7cebff6e76438f80617c347e3cef7
Paul Robertson <paul.robertson@simulation-systems.co.uk>
2017-03-16 17:47:11 +0000

    click handling

--- ng/controllers/cards.ctrl.js
+++ ng/controllers/cards.ctrl.js
@@ -5,5 +5,16 @@ angular.module('app')
     $scope.cards2 = cards.splice(0,3)
     $scope.cards3 = cards.splice(0,3)
   })
-})
 
+  $scope.click = function(row, col) {
+    if (row==0) {
+      $scope.cards1[col].selected = !$scope.cards1[col].selected
+    }
+    else if (row==1) {
+      $scope.cards2[col].selected = !$scope.cards2[col].selected
+    }
+    else if (row==2) {
+      $scope.cards3[col].selected = !$scope.cards3[col].selected
+    }
+  }
+})
